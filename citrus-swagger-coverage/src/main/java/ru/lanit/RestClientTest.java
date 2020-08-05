9b440b27b91c8b573d363ba7fd327793a3919b1c
kolyan.samokhin@mail.ru <36818428+NikolaysSamokhin@users.noreply.github.com>
2020-08-05 18:43:18 +0300

    Added interfaces for processing url params

--- citrus-swagger-coverage/src/main/java/ru/lanit/RestClientTest.java
+++ citrus-swagger-coverage/src/main/java/ru/lanit/RestClientTest.java
@@ -59,7 +59,10 @@ public class RestClientTest extends TestNGCitrusTest {
     @CitrusTest(name = "test3")
     public void petStorePathParamTest(@Optional @CitrusResource TestRunner testRunner) throws IOException {
         Map<String, Object> pathParams = new HashMap<>();
-        pathParams.put("id", "1");
+        pathParams.put("{id}", "1");
+        pathParams.put("{v2}", "1");
+
+
         testRunner.http(action -> {
             action.client("httpClient")
                     .send()
--- citrus-swagger-coverage/src/main/java/ru/lanit/utils/InterceptorHandler.java
+++ citrus-swagger-coverage/src/main/java/ru/lanit/utils/InterceptorHandler.java
@@ -1,9 +1,14 @@
 package ru.lanit.utils;
 
 import org.springframework.http.HttpHeaders;
+
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriTemplate;
 import ru.lanit.interfaces.HttpCitrusSpecHandler;
 
-import java.util.Arrays;
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InterceptorHandler implements HttpCitrusSpecHandler {
 
@@ -14,7 +19,10 @@ public class InterceptorHandler implements HttpCitrusSpecHandler {
 
     @Override
     public String changePathParam(String path, HttpHeaders headers) {
-//        Arrays.stream(headers).filter(x->x.entrySet().)
-        return null;
+
+        Map<String, String> parameters = headers.entrySet().stream().filter(x -> x.getKey().startsWith("{"))
+                .collect(Collectors.toMap(k -> k.getKey(), v -> v.getValue().get(0)));
+
+        return path;
     }
 }
